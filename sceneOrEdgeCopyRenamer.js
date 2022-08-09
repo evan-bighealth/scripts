687060a2b6d94ea8ebf18b26030b87d80f9539d6
Evan Dana <evan.dana@bighealth.com>
2022-08-09 14:44:20 -0400

    Addressing edge renaming

--- sceneOrEdgeCopyRenamer.js
+++ sceneOrEdgeCopyRenamer.js
@@ -4,6 +4,8 @@
  * This is intended to be used when renaming items copied over from one SSG to another.
  * e.g. renmaing TC_rapidpractice_100PercentOffer_copy_e41c8318f321457c864af8099ea3059f to be MDD_TC_rapidpractice_100PercentOffer
  * 
+ * NOTE: for Edges, you have to manually press Save. Could use some refactoring
+ * 
  * 1. Navigate to an SSG and double click on a SceneSet or Edge that has _copy_<hash> in the reference
  * 2. Copy the full contents of the script and paste into Chrome's DevTools
  * 3. Run
@@ -11,35 +13,96 @@
  *    - Save Changes: `replaceDummyNameContent(true)`
  */
 
+const ssType = 'SceneSet';
+const edgeType = 'Edge';
+const ssFormId = '#scene-set-editor-form';
+const edgeFormId = '#form-condition-editor';
+
+function getFormType () {
+    if ([...$(ssFormId)]?.length) {
+        return ssType;
+    } else if ([...$(edgeFormId)]?.length) {
+        return edgeType;
+    }
+}
+
+function getDomReference() {
+    switch (getFormType()) {
+        case ssType:
+            return getSSDomReference();
+        case edgeType:
+            return getEdgeDomReference();
+    }
+}
 function getSSDomReference() {
-    const el = [...$('#scene-set-editor-form [name=reference]')];
+    const el = [...$(`${ssFormId} [name=reference]`)];
+    return el && el[0] ? el[0] : null;
+}
+function getEdgeDomReference() {
+    const el = [...$(`${edgeFormId} [name=reference]`)];
     return el && el[0] ? el[0] : null;
 }
 function getSsSaveButtonDomReference() {
-    const el = [...$('#scene-set-editor-form [type=submit]')];
+    let formId;
+    switch (getFormType()) {
+        case ssType:
+            formId = ssFormId;
+            break;
+        case edgeType:
+            formId = edgeFormId;
+            break;
+    }
+    const el = [...$(`${formId} [type=submit][value="Save"]`)];
     return el && el[0] ? el[0] : null;
+}
+function getEdgeValidateButtonDomReference() {
+
+}
+function getValueOfDomRef(domRef) {
+    return domRef.value;
 }
 function setReplaceStringForRef(domRef, replaceStr) {
     return domRef.value = replaceStr;
 }
-async function saveSsOrEdge() { return getSsSaveButtonDomReference().click(); }
+async function saveSsOrEdge() { 
+    if (getFormType() === edgeType) {
+        // click on Advanced
+        const advancedTabButton = [...$(`${edgeFormId} a[href="#tab-advanced-editor"]`)][0];
+        console.log('advancedTabButton', advancedTabButton);
+        await advancedTabButton.click();
+        // wait 3 seconds
+        console.log('waiting for tab transition')
+        const pausePromise = new Promise((resolve) => (setTimeout(resolve, 1000)));
+        await pausePromise;
+        console.log('pressing validate button')
+        // press Validate
+        const validateButton = [...$(`${edgeFormId} a[data-action="validate"][data-target="advanced"]`)][0];
+        console.log('validateButton', validateButton);
+        await validateButton.click();
+        // press Save (handled next)
+        console.log('save will fail for some reason')
+        console.log('PRESS SAVE MANUALLY');
+    } else {
+        await getSsSaveButtonDomReference().click();
+    }
+}
 
 const copyHashRegex = new RegExp(/_copy_[\w\d]+/);
 const prefix = 'MDD_';
 function doesRefContainCopyHashString (domRef) {
-    return domRef.value.match(copyHashRegex);
+    return getValueOfDomRef(domRef).match(copyHashRegex);
 }
 function getReplaceStringForRef (domRef) {
-    return `${prefix}${domRef.value.replace(copyHashRegex, '')}`;
+    return `${prefix}${getValueOfDomRef(domRef).replace(copyHashRegex, '')}`;
 }
 
 async function replaceDummyNameContent(performDataSave = false) {
     console.log(performDataSave ? '⚠️ Will attempt to save changes' : 'Running in dry run mode')
-    const domRef = getSSDomReference();
+    const domRef = getDomReference();
     if (doesRefContainCopyHashString(domRef)) {
         const replaceStr = getReplaceStringForRef(domRef);
         setReplaceStringForRef(domRef, replaceStr);
-        console.log(`String ${domRef.value} is a candidate for replacement with ${replaceStr}`);
+        console.log(`String ${getValueOfDomRef(domRef)} is a candidate for replacement with ${replaceStr}`);
         if (performDataSave) {
             console.log('⚠️ Saving...');
             return saveSsOrEdge()
